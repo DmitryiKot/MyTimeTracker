a983d5593dfc8fe8b776eb3c3a014af7e0fdab85
Dmitryikot <dmitryi.kot@gmai.com>
2020-03-15 20:14:50 +0300

    Add profile and registration.

--- mytimetracker/trackerApp/models.py
+++ mytimetracker/trackerApp/models.py
@@ -1,3 +1,12 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Profile for {}".format(self.user.username)
--- mytimetracker/trackerApp/urls.py
+++ mytimetracker/trackerApp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.contrib.auth import views
 
+from . import views as my_views
+
 app_name = 'trackerApp'
 
 urlpatterns = [
@@ -8,4 +10,8 @@ urlpatterns = [
          name='login'),
     path('logout/', views.LogoutView.as_view(),
          name='logout'),
+    path('profile/', my_views.view_profile,
+         name='profile'),
+    path('register/', my_views.register,
+         name='register'),
 ]
